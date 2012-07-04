4dbd9653438298eab68d146b67a2cd014047f47f
N.Nakasato <nakasato@u-aizu.ac.jp>
2012-07-05 06:51:07 +0900

    Minor fix

--- conf.rb
+++ conf.rb
@@ -1,6 +1,5 @@
 #################################################################################
-#EIGEN  = "/opt/include"
-EIGEN  = "/home/nakasato/include"
+EIGEN  = "/opt/include"
 OCLSDK = "/opt/AMDAPP" 
 CFLAGS = "-std=c++0x -Wall -Wno-unused-result -I#{EIGEN} -I#{OCLSDK}/include -IOTOO #{INC}"
 LFLAGS = "-lnetcdf -lnetcdf_c++ -lgsl -lgslcblas -lm -lOpenCL -L#{OCLSDK}/lib/x86_64"
